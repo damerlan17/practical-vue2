e5b615c3808531f66c5450b3fcf70fb87cd4cae6
damerlan17 <uchunakdamelan@gmail/com>
2026-02-21 12:35:34 +0700

    сделал чтобы можно было добавлять в списки

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,16 @@ createApp({
                         { text: 'Пункт 2', completed: false },
                         { text: 'Пункт 3', completed: false }
                     ] },
-                { id: 2, title: 'заметка 2', col: 2, items: [] },
-                { id: 3, title: 'заметка 3', col: 3, items: [] },
+                { id: 2, title: 'заметка 2', col: 2, items: [
+                        { text: 'Пункт 1', completed: false },
+                        { text: 'Пункт 2', completed: false },
+                        { text: 'Пункт 3', completed: false }
+                    ] },
+                { id: 3, title: 'заметка 3', col: 3, items: [
+                        { text: 'Пункт 1', completed: false },
+                        { text: 'Пункт 2', completed: false },
+                        { text: 'Пункт 3', completed: false }
+                    ] },
 
             ],
 
@@ -23,6 +31,18 @@ createApp({
     },
 
 
-    methods: {}
+    methods: {
+        addItem(card) {
+            if (card.items.length < 5) {
+                card.items.push({ text: '', completed: false });
+            }
+        },
+        removeItem(index, card) {
+            if (card.items.length > 3) {
+                card.items.splice(3, 1);
+            }
+        },
+
+    }
 
 }).mount('#app');
